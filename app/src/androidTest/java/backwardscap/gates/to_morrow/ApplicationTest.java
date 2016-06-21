10a55e6430214f95824654eceb397c46f0040c1e
Sam Gates <skiracer1292@gmail.com>
2016-06-21 13:30:56 -0400

    Initial commit

---
+++ app/src/androidTest/java/backwardscap/gates/to_morrow/ApplicationTest.java
@@ -0,0 +1,13 @@
+package backwardscap.gates.to_morrow;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
---
+++ app/src/main/java/backwardscap/gates/to_morrow/db/TaskContract.java
@@ -0,0 +1,23 @@
+package backwardscap.gates.to_morrow.db;
+
+/**
+ * Created by Sam on 6/20/2016.
+ */
+
+import android.provider.BaseColumns;
+
+public class TaskContract {
+
+    public static final String DB_NAME = "backwardscap.gates.to_morrow.db";
+    public static final int DB_VERSION = 1;
+
+    public class TaskEntry implements BaseColumns {
+
+        public static final String TABLE = "tasks";
+
+        public static final String COL_TASK_TITLE = "title";
+
+    }
+
+
+}
